f193e51ce5a4bd7d5234ab9f24bbff3869f5eb64
kifran72 <cr91700kifran@gmail.com>
2017-04-27 14:02:22 +0200

    Add files via upload

--- Project-SHIFOUMI-master/Shifumi.cpp
+++ Project-SHIFOUMI-master/Shifumi.cpp
@@ -2,125 +2,129 @@
 
 using namespace std;
 
-// Pierre==1 ; Feuille==2; Sciseau==3;
 
 class shifumi
 {
   private:
-        int NbManches = 3; //Nombre de manche total
-        int Manches = 0;
-        int STARTTOUCHE = 0;
-        int Resultat;           // True pou joueur 1 et False Joueur 2
-        int a = 0;               // Borne exclue min du rand
-        int b = 4;  
+        int NbManches; //Nombre de manche total
+        int Manches;
+        int STARTTOUCHE;
+        int Resultat;           // True pour joueur 1 et False Joueur 2
+        int a;               // Borne exclue min du rand
+        int b;  
     
         int Regle(string ChoixJoueur1, string ChoixJoueur2) // Fonction de règle de jeu qui deffini le gagnant de la partie.
             {
-                
-                if (ChoixJoueur1 == "pierre" && ChoixJoueur2 == "feuille") // Pierre vs Feuille  Pierre Lose.
+                //pierre 1 , feuille 2, sciseau 3 
+                if (ChoixJoueur1 == "1" && ChoixJoueur2 == "2") // Pierre vs Feuille  Pierre Lose.
                 {
                     Resultat = 0;
                 }
-                else if (ChoixJoueur1 == "pierre" && ChoixJoueur2 == "feuille") // Pierre vs Sciseau  Pierre Win.
+                else if (ChoixJoueur1 == "1" && ChoixJoueur2 == "2") // Pierre vs Sciseau  Pierre Win.
                 {
                     Resultat = 1;
                 }
 
-                if (ChoixJoueur1 == "feuille" && ChoixJoueur2 == "pierre") // Feuille vs Pierre  Feuille Win.
+                if (ChoixJoueur1 == "2" && ChoixJoueur2 == "pierre") // Feuille vs Pierre  Feuille Win.
                 {   
                     Resultat = 1;
                 }
-                else if (ChoixJoueur1 == "feuille" && ChoixJoueur2 == "sciseau") // Feuille vs Sciseau  Feuille Lose.
+                else if (ChoixJoueur1 == "2" && ChoixJoueur2 == "3") // Feuille vs Sciseau  Feuille Lose.
                 {
                     Resultat = 0;
                 }
 
-                if (ChoixJoueur1 == "sciseau" && ChoixJoueur2 == "pierre") // Sciseau vs Pierre  Sciseau Lose.
+                if (ChoixJoueur1 == "3" && ChoixJoueur2 == "1") // Sciseau vs Pierre  Sciseau Lose.
                 {
                     Resultat = 0;
                 }
-                else if (ChoixJoueur1 == "sciseau" && ChoixJoueur2 == "feuille") // Sciseau vs Feuille  Sciseau Win.
+                else if (ChoixJoueur1 == "3" && ChoixJoueur2 == "2") // Sciseau vs Feuille  Sciseau Win.
                 {
                     Resultat = 1;
                 }
 
-            
+            return Resultat;
             }
           
             
-        int Score(int    Resultat) // Fonction de vie du joueur avec un bolléen en paramètre.
+        int Score(int Resultat,int Player1Name, int Player2Name ) // Fonction de vie du joueur avec un bolléen en paramètre.
             {
-                
+              int ScoreChoixJoueur1; 
+              int ScoreChoixJoueur2; 
+              int Manche; 
                 if (Resultat == 1)
                 {
                     ScoreChoixJoueur1 = ScoreChoixJoueur1 + 1;
-                    cout >> Player1Name "Gagne la manche";
+                    cout << Player1Name <<": Gagne la manche" << endl;
                     Manche++;
                 }
                 else
                 {
                     ScoreChoixJoueur2 = ScoreChoixJoueur2 + 1;
-                    cout >> Player2Name "Gagne la manche ";
+                    cout << Player2Name << ": Gagne la manche " << endl;
                     Manche++;
                 }
+                return Manche;
+                return ScoreChoixJoueur1;
+                return ScoreChoixJoueur2;
             }
 
-        int Win(int NbManches, int Manche, int ScoreChoixJoueur1, int ScoreChoixJoueur2) // Fonction de recherche si le jeu est fini et cherche le Winner
+        int Win(int NbManches, int Manche, int ScoreChoixJoueur1, int ScoreChoixJoueur2, int Player1Name, int Player2Name) // Fonction de recherche si le jeu est fini et cherche le Winner
             {
                 if (NbManches == Manche)
                 {
                     if (ScoreChoixJoueur1 > ScoreChoixJoueur2)
                     {
-                        cout >> Player1Name "Gagne le jeu";
+                        cout << Player1Name << " Gagne le jeu !" << endl;
                     }
                     else
                     {
-                        cout >> Player2Name "Gagne le jeu";
+                        cout << Player2Name << " Gagne le jeu !" << endl;
                     }
                 }
             }
 
             
 
-        int Boot(int a, int b)
-            {
-                int ChoixJoueur2 = rand() % (b - a) + a;
-            }
+        // int Bot(int a, int b)
+        //     {
+        //         int ChoixJoueur2 = rand() % (b - a) + a;
+        //     }
         
-        
 
-    public:
-
-
-   
-
-
+public:
 
     int Initialisation() //Fonction d'Initialisation de la variable NbManches et du nom des joueurs.
             {
-                cout << "Saisir le nombre de manches pour gagner";
+                int Player1Name;
+                int Player2Name;
+                int NbManches;
+                cout << "Saisir le nombre de manches pour gagner" << endl;
                 cin >> NbManches;
 
                 while (NbManches >= 30 && NbManches <= 0)
                 {
-                    cout << "Saisir le nombre de manches pour gagner";
+                    cout << "Saisir le nombre de manches pour gagner" << endl;
                     cin >> NbManches;
                 }
 
-                cout << "Nom du joueur 1";
+                cout << "Nom du joueur 1" << endl;
                 cin >> Player1Name;
-                cout << "Nom du joueur 1";
+                cout << "Nom du joueur 2" << endl;
                 cin >> Player2Name;
-                return 0;
+            
+                return NbManches;
+                return Player1Name;
+                return Player2Name;
             }
 
     int StartGame()
             {
+                int STARTTOUCHE;
+
                 cout << "Hello , Bienvenue sur le jeu du shifumi" << endl;
                 cout << "Choix du mod de jeu " << endl;
-                cout << "Joueur vs Joeur TOUCHE 1" << endl;
-                cin >> STARTTOUCHE;
-                cout << "joueur VS BOOT TOUCHE 2" << endl;
+                cout << "1. Joueur vs Joueur" << endl << "2. joueur VS BOT" << endl;
                 cin >> STARTTOUCHE;
 
                 if (STARTTOUCHE == 1)
@@ -128,32 +132,22 @@ class shifumi
                     Initialisation();
                 }
                 else if (STARTTOUCHE == 2)
-                {
+                {   
+                    cin.ignore();
                 }
                 else
                     {
                         cin.ignore();
                         return 0;
                     }
+                    return STARTTOUCHE;
             }
     
     
 };
 
-class joueur
-{
-    private:
 
-        int ChoixJoueur1 = 0;         //Choix Choixjoueur1
-        int ChoixJoueur2 = 0;         //Choix Choixjoueur2
-        string Player1Name;      //Nom du joueur 1
-        string Player2Name;      //Nom du joueur 2
-        int ScoreChoixJoueur1 = 0;    //Score du joueur 1
-        int ScoreChoixJoueur2 = 0;    //Score du joueur 2
 
-    public:
-    
-}
 
 
 
--- Project-SHIFOUMI-master/main.cpp
+++ Project-SHIFOUMI-master/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-
+#include "Shifumi.cpp"
 using namespace std;
 
 
 
 int main()
     {
-
-     
-        
+    shifumi Shifumi;
+    Shifumi.StartGame();
+    return 0; 
     }
 
     
-    
-
