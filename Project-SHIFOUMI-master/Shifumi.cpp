95c00d20a15a9efb761f3f1de6fab6d5706375ee
Sywana <b0benito@free.fr>
2017-04-27 18:58:46 +0200

    Add files via upload

--- Project-SHIFOUMI-master/Shifumi.cpp
+++ Project-SHIFOUMI-master/Shifumi.cpp
@@ -92,15 +92,8 @@ class Shifumi
                 }
             }
 
-            
-
-        // int Bot(int a, int b)
-        //     {
-        //         int ChoixJoueur2 = rand() % (b - a) + a;
-        //     }
-        
-
 public:
+
   Shifumi() {
             NbManches = 1;
             Manches = 1;
@@ -110,7 +103,6 @@ public:
         }
  int StartGame()
             {
-                int STARTTOUCHE;
 
                 cout << "Hello , Bienvenue sur le jeu du shifumi" << endl;
                 cout << "Choix du mod de jeu " << endl;
@@ -119,11 +111,11 @@ public:
 
                 if (STARTTOUCHE == 1)
                 {
-                    Initialisation();
+                    Initialisation(STARTTOUCHE);
                 }
                 else if (STARTTOUCHE == 2)
                 {   
-                    cin.ignore();
+                    Initialisation(STARTTOUCHE);
                 }
                 else
                     {
@@ -132,43 +124,79 @@ public:
                     }
                     return STARTTOUCHE;
             }
-    void Initialisation() //Fonction d'Initialisation de la variable NbManches et du nom des joueurs.
+    void Initialisation(int STARTTOUCHE) //Fonction d'Initialisation de la variable NbManches et du nom des joueurs.
             {
 
+		if (STARTTOUCHE = 1) {
+					cout << "Saisir le nombre de manches pour gagner" << endl;
+					cin >> NbManches;
 
-                cout << "Saisir le nombre de manches pour gagner" << endl;
-                cin >> NbManches;
+					while (NbManches >= 30 && NbManches <= 0)
+					{
+						cout << "Saisir le nombre de manches pour gagner" << endl;
+						cin >> NbManches;
+					}
 
-                while (NbManches >= 30 && NbManches <= 0)
-                {
-                    cout << "Saisir le nombre de manches pour gagner" << endl;
-                    cin >> NbManches;
-                }
+					cout << "Nom du joueur 1" << endl;
+					cin >> Player1Name;
+					cout << "Nom du joueur 2" << endl;
+					cin >> Player2Name;
 
-                cout << "Nom du joueur 1" << endl;
-                cin >> Player1Name;
-                cout << "Nom du joueur 2" << endl;
-                cin >> Player2Name;
+					Game(STARTTOUCHE);
+				}
 
-                Game();
+		else (STARTTOUCHE = 2) {
+			cout << "Saisir le nombre de manches pour gagner" << endl;
+			cin >> NbManches;
+			Player2Name = "BOT";
+
+			while (NbManches >= 30 && NbManches <= 0)
+			{
+				cout << "Saisir le nombre de manches pour gagner" << endl;
+				cin >> NbManches;
+			}
+
+			cout << "Nom du joueur 1" << endl;
+			cin >> Player1Name;
+
+			Game(STARTTOUCHE);
+		}
+
             }
 
    
-    int Game()
+    void Game(int STARTTOUCHE)
     {   
         int ChoixJoueur1;
         int ChoixJoueur2;
 
+		if (STARTTOUCHE = 1) {
 
-        for(Manches=1 ; Manches<=NbManches ; Manches++)
-        {
-            cout << Player1Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
-            cin >> ChoixJoueur1;
-            cout << Player2Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
-            cin >> ChoixJoueur2;
+			for (Manches = 1; Manches <= NbManches; Manches++)
+			{
+				cout << Player1Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
+				cin >> ChoixJoueur1;
+				cout << Player2Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
+				cin >> ChoixJoueur2;
 
-            Regle(ChoixJoueur1, ChoixJoueur2);
-        }
+				Regle(ChoixJoueur1, ChoixJoueur2);
+			}
+
+		}
+		else (STARTTOUCHE = 2) {
+
+			for (Manches = 1; Manches <= NbManches; Manches++)
+			{
+				cout << Player1Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
+				cin >> ChoixJoueur1;
+				cout << Player2Name << endl << "1. pierre" << endl << "2. Feuille" << endl << "3. Ciseau" << endl;
+
+				ChoixJoueur2 = rand() % (b - a) + a;
+
+				Regle(ChoixJoueur1, ChoixJoueur2);
+			}
+
+		}
         
     }
 
@@ -180,5 +208,6 @@ public:
 
 
 
-
+	//systeme(clear) linux/MAC
+	//systeme(cls) windows
     // end
