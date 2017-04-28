ae80ae04e056b21a24b98631eff7d69ec00db2b3
kifran72 <cr91700kifran@gmail.com>
2017-04-28 22:20:40 +0200

    Add files via upload

---
+++ Project-SHIFOUMI/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Shifumi.cpp"
+using namespace std;
+
+
+
+int main()
+    {
+    Shifumi shifumi;
+    shifumi.PrintFile();
+    shifumi.StartGame();
+
+    return 0; 
+    }
+
+    
